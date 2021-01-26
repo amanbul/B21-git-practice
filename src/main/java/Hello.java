0eebff3980ade9a611c3272ccf900617fb9e2594
Amannisa Sawuti <asawutiwupuer.15@gmail.com>
2021-01-26 15:16:19 -0500

    movie1 branch updated

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello b21");
         System.out.println("B21 FUTURE SDET LEARING GITHUB");
 
     }
